038d4cbe7bf1db2e7ddb83fbb13bf8b49edeedb5
mcflugen <mcflugen@gmail.com>
2015-10-03 11:00:22 -0600

    Search for esmf.mk file.

--- conda-recipes/esmpy/build.sh
+++ conda-recipes/esmpy/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ESMFMKFILE=$PREFIX/lib/libO/Linux.gfortran.64.mpiuni.default/esmf.mk
+ESMFMKFILE=$(find $PREFIX/lib/libO/* -name esmf.mk)
 
 cd src/addon/ESMPy
 $PYTHON setup.py build --ESMFMKFILE=$ESMFMKFILE install --prefix=$PREFIX
