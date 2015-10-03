4274a6b0793fa52d03875af08dce7e994a996d3d
mcflugen <mcflugen@gmail.com>
2015-10-03 08:41:17 -0600

    Get compilers from babel-config command.

--- conda-recipes/cca-spec-babel/build.sh
+++ conda-recipes/cca-spec-babel/build.sh
@@ -1,7 +1,12 @@
 BABEL_CONFIG=$PREFIX/bin/babel-config
-export USER=huttone
+export CC=$($BABEL_CONFIG --query-var=CC)
+export CXX=$($BABEL_CONFIG --query-var=CXX)
+
+export USER=nobody
 export SHELL=/bin/bash
 
-./configure --prefix=$PREFIX --disable-contrib --with-babel-config=$BABEL_CONFIG
+./configure --prefix=$PREFIX --disable-contrib \
+  --with-babel-config=$BABEL_CONFIG --with-libxml2=$PREFIX
+
 make all
 make install
