8bd816612d527f2de5d2b9556f43617e369a2702
mcflugen <mcflugen@gmail.com>
2015-10-03 08:42:51 -0600

    Get compilers from babel-config command.

--- conda-recipes/ccaffeine/build.sh
+++ conda-recipes/ccaffeine/build.sh
@@ -1,6 +1,6 @@
-export
-
-LDFLAGS="-ltinfo"
+BABEL_CONFIG=$PREFIX/bin/babel-config
+export CC=$($BABEL_CONFIG --query-var=CC)
+export CXX=$($BABEL_CONFIG --query-var=CXX)
 
 ./configure --prefix=$PREFIX --without-mpi --with-boost=$PREFIX/include
 make
