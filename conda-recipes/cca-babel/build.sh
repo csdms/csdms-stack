979ebaaa0d86f79695683d58806ae2613edc44ef
mcflugen <mcflugen@gmail.com>
2015-10-03 08:39:53 -0600

    Changed to use clang if it's available.

--- conda-recipes/cca-babel/build.sh
+++ conda-recipes/cca-babel/build.sh
@@ -1,12 +1,25 @@
-export CC=$(which gcc)
-export CXX=$(which g++)
-export PYTHON=$PREFIX/bin/python
-export JAVA=/usr/java/default/bin/java
-export JAVAPREFIX=/usr/java/default
+export CC=$(which clang)
+export CXX=$(which clang++)
+
+if [[ -z $CC ]]; then
+  export CC=$(which gcc)
+  export CXX=$(which g++)
+fi
+
+if [ $(uname) == Darwin ]; then
+  export JAVAPREFIX=$(/usr/libexec/java_home)
+  export JAVA=$JAVAPREFIX/bin/java
+else
+  export JAVA=/usr/java/default/bin/java
+  export JAVAPREFIX=/usr/java/default
+fi
+
 export FC=$(which gfortran)
 export F77=$(which gfortran)
 export F90=$(which gfortran)
 export F03=$(which gfortran)
+
+export PYTHON=$PREFIX/bin/python
 
 ./configure --prefix=$PREFIX --disable-documentation --disable-java
 make all -j4
