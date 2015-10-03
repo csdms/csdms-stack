e047c0cd058e42b63c101cc2cdac000404fd54cb
mcflugen <mcflugen@gmail.com>
2015-10-03 11:01:00 -0600

    Removed absolute path to babel-config.

--- conda-recipes/csdms-components/build.sh
+++ conda-recipes/csdms-components/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-export CC=$($PREFIX/babel-config --query-var=CC)
-export CXX=$($PREFIX/babel-config --query-var=CXX)
+export CC=$(babel-config --query-var=CC)
+export CXX=$(babel-config --query-var=CXX)
 export USER=nobody
 
 echo "- https://github.com/mcflugen/sedflux,add-function-pointers" > _repos.yaml
@@ -9,4 +9,5 @@ echo "- https://github.com/csdms/bmi-python" >> _repos.yaml
 
 $PREFIX/bin/bmi-babel-fetch --file=_repos.yaml --prefix=$PREFIX > _models.yaml
 $PREFIX/bin/bmi-babel-make _models.yaml
+
 cd csdms && ./configure --prefix=$PREFIX && make all install
