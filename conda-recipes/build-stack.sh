c8125ace4e574282041299b24b486c193c62dea7
mcflugen <mcflugen@gmail.com>
2015-10-08 09:48:34 -0600

    Build everything.

--- conda-recipes/build-stack.sh
+++ conda-recipes/build-stack.sh
@@ -1,21 +1,20 @@
 #! /bin/bash
 
 #conda build gettext
-#conda build glib && \
-#conda build libffi && \
-#conda build netcdff && \
 
+conda build libffi && \
+conda build glib && \
+conda build netcdff && \
 conda build libxml2 && \
 conda build boost-headers && \
 conda build chasm && \
-conda build cca-babel
-
-#conda build cca-spec-babel && \
-#conda build ccaffeine && \
-#conda build cca-bocca && \
-#conda build bmi-babel && \
-#conda build csdms-components && \
-#conda build esmf && \
-#conda build esmpy && \
-#conda build coupling && \
-#conda build wmt-exe
+conda build cca-babel && \
+conda build cca-spec-babel && \
+conda build ccaffeine && \
+conda build cca-bocca && \
+conda build bmi-babel && \
+conda build csdms-components && \
+conda build esmf && \
+conda build esmpy && \
+conda build coupling && \
+conda build wmt-exe
