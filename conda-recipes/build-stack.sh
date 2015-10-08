1c0d8b57c8d2798ae439dfd3239e6cd00f43a078
mcflugen <mcflugen@gmail.com>
2015-10-08 08:08:01 -0600

    Changed to just build babel for testing.

--- conda-recipes/build-stack.sh
+++ conda-recipes/build-stack.sh
@@ -8,13 +8,14 @@
 conda build libxml2 && \
 conda build boost-headers && \
 conda build chasm && \
-conda build cca-babel && \
-conda build cca-spec-babel && \
-conda build ccaffeine && \
-conda build cca-bocca && \
-conda build bmi-babel && \
-conda build csdms-components && \
-conda build esmf && \
-conda build esmpy && \
-conda build coupling && \
-conda build wmt-exe
+conda build cca-babel
+
+#conda build cca-spec-babel && \
+#conda build ccaffeine && \
+#conda build cca-bocca && \
+#conda build bmi-babel && \
+#conda build csdms-components && \
+#conda build esmf && \
+#conda build esmpy && \
+#conda build coupling && \
+#conda build wmt-exe
