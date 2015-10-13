4b049e0d410e9d470ffb35813b4e83ff5ca37dc3
mcflugen <mcflugen@gmail.com>
2015-10-13 12:11:47 -0600

    Changed to build with clang on Mac.

--- conda-recipes/libxml2/build.sh
+++ conda-recipes/libxml2/build.sh
@@ -1,15 +1,12 @@
-#!/bin/bash
-
-#export CC=$(which clang)
-#export CXX=$(which clang++)
+if [ `uname` == Darwin ]; then
+  export EXTRA_ARGS="--without-threads"
+  export CC=$(which clang)
+  export CXX=$(which clang++)
+fi
 
 if [[ -z $CC ]]; then
   export CC=$(which gcc)
   export CXX=$(which g++)
-fi
-
-if [ `uname` == Darwin ]; then
-    export EXTRA_ARGS="--without-threads"
 fi
 
 ./configure $EXTRA_ARGS --prefix=$PREFIX
