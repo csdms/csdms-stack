a3320e8cc8df364ea72d870a20c972a4bad528fb
mcflugen <mcflugen@gmail.com>
2015-10-21 10:37:52 -0600

    Use gcc rather than clang on Mac.

--- conda-recipes/glib/build.sh
+++ conda-recipes/glib/build.sh
@@ -1,7 +1,7 @@
-if [ `uname` == Darwin ]; then
-  export CC=$(which clang)
-  export CXX=$(which clang++)
-fi
+#if [ `uname` == Darwin ]; then
+#  export CC=$(which clang)
+#  export CXX=$(which clang++)
+#fi
 
 if [[ -z $CC ]]; then
   export CC=$(which gcc)
@@ -13,7 +13,8 @@ export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
 export CFLAGS="-I$PREFIX/include"
 export CPPFLAGS="-I$PREFIX/include"
 export CXXFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib64 -L$PREFIX/lib"
+export LDFLAGS="-L$PREFIX/lib"
+#export LDFLAGS="-L$PREFIX/lib64 -L$PREFIX/lib"
 export LIBFFI_CFLAGS=" "
 export LIBFFI_LIBS="-lffi"
 export DYLD_LIBRARY_PATH="$PREFIX/lib"
