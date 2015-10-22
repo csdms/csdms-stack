4827300bd798e623e738a5617b980f55512f80b9
mcflugen <mcflugen@gmail.com>
2015-10-22 12:23:15 -0600

    Moved tests into script, test_stack.sh.

---
+++ .ci/travis/test_stack.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+if [ $CONDA_RECIPES == "runtime" ]; then
+  echo "Building the CSDMS runtime"
+  conda install csdms-models coupling -c csdms/channel/nightly || exit -1
+  python -c 'from cmt.components import Hydrotrend; Hydrotrend()'; || exit -1
+else
+  echo "Building the CSDMS recipes: $CONDA_RECIPES"
+  conda build $CONDA_RECIPES || exit -1
+  export CONDA_FILES_TO_UPLOAD=$(conda build --output --python=$TRAVIS_PYTHON_VERSION --numpy=$NUMPY_VERSION $CONDA_RECIPES) || exit -1
+fi
