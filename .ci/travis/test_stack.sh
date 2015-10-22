0979b4656b044381ade6f1dcf143ab0a2eb38b1e
mcflugen <mcflugen@gmail.com>
2015-10-22 12:26:44 -0600

    Fixed typo.

--- .ci/travis/test_stack.sh
+++ .ci/travis/test_stack.sh
@@ -3,7 +3,7 @@
 if [ $CONDA_RECIPES == "runtime" ]; then
   echo "Building the CSDMS runtime"
   conda install csdms-models coupling -c csdms/channel/nightly || exit -1
-  python -c 'from cmt.components import Hydrotrend; Hydrotrend()'; || exit -1
+  python -c 'from cmt.components import Hydrotrend; Hydrotrend()' || exit -1
 else
   echo "Building the CSDMS recipes: $CONDA_RECIPES"
   conda build $CONDA_RECIPES || exit -1
