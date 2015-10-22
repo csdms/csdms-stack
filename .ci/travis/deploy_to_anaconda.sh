31af14817e42baf7286245b56621b7298d44bf30
mcflugen <mcflugen@gmail.com>
2015-10-22 15:48:38 -0600

    Move to conda-recipes when deploying.

--- .ci/travis/deploy_to_anaconda.sh
+++ .ci/travis/deploy_to_anaconda.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 if [ "$CONDA_RECIPES" != "runtime" ]; then
+  cd conda-recipes
+
   CONDA_FILES_TO_UPLOAD=$(conda build --output --python=$TRAVIS_PYTHON_VERSION --numpy=$NUMPY_VERSION $CONDA_RECIPES)
 
   echo Deploying $CONDA_FILES_TO_UPLOAD to $CONDA_USERNAME
