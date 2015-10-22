4bba4dd7d3ba7042bbad1feffb770b98b21a9a88
mcflugen <mcflugen@gmail.com>
2015-10-22 16:13:49 -0600

    Added csdms/channel/nightly to build command.

--- .ci/travis/test_stack.sh
+++ .ci/travis/test_stack.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
 if [ $CONDA_RECIPES == "runtime" ]; then
-  echo "Building the CSDMS runtime"
   conda install csdms-models coupling -c csdms/channel/nightly || exit -1
   python -c 'from cmt.components import Hydrotrend; Hydrotrend()' || exit -1
 else
-  echo "Building the CSDMS recipes: $CONDA_RECIPES"
-  (cd conda-recipes && conda build $CONDA_RECIPES) || exit -1
+  (cd conda-recipes && conda build $CONDA_RECIPES -c csdms/channel/nightly) || exit -1
 fi
