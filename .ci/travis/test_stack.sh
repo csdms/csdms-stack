13c34282300dc3724ef209e502ac058d4df7ccd0
mcflugen <mcflugen@gmail.com>
2015-12-01 12:19:27 -0700

    Changed to get requirements from csdms channel.

--- .ci/travis/test_stack.sh
+++ .ci/travis/test_stack.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 if [ $CONDA_RECIPES == "runtime" ]; then
-  conda install csdms-models coupling -c csdms/channel/nightly || exit -1
+  conda install csdms-hydrotrend csdms-rafem csdms-cem coupling -c csdms || exit -1
   python -c 'from cmt.components import Hydrotrend; Hydrotrend()' || exit -1
 else
-  (cd conda-recipes && conda build $CONDA_RECIPES -c csdms/channel/travis) || exit -1
+  (cd conda-recipes && conda build $CONDA_RECIPES -c csdms) || exit -1
 fi
