e513ae02dcae69a18f9b219989e61139d949549d
mcflugen <mcflugen@gmail.com>
2016-09-30 20:07:46 -0600

    Added FCFLAGS and FFLAGS to fix rpath error.

--- conda-recipes/cca-babel/build.sh
+++ conda-recipes/cca-babel/build.sh
@@ -13,6 +13,11 @@ fi
 if [[ -z $FC ]]; then
   export FC=$(which gfortran)
 fi
+
+export CC=$(which gcc)
+export CXX=$(which g++)
+export FC=$(which gfortran)
+
 export F77=$FC
 export F90=$FC
 export F03=$FC
@@ -32,7 +37,20 @@ export PATH=$JAVAPREFIX/bin:$PATH
 
 ln -s "$PREFIX/lib" "$PREFIX/lib64"
 
-./configure --prefix=$PREFIX --disable-documentation
+export FCFLAGS="-Wl,-rpath,${PREFIX}/lib"
+export FFLAGS="-Wl,-rpath,${PREFIX}/lib"
+
+# export LC_RPATH=$PREFIX/lib
+# export DYLD_LIBRARY_PATH=$PREFIX/lib
+# export LD_LIBRARY_PATH=$PREFIX/lib
+
+# install_name_tool -id @rpath/./libgfortran.3.dylib $PREFIX/bin/gfortran
+
+./configure --prefix=$PREFIX --disable-documentation \
+  --with-libparsifal=$PREFIX --with-ltdl-include=$PREFIX/include \
+  --with-ltdl-lib=$PREFIX/lib --with-libxml2=$PREFIX --with-chasm=$PREFIX \
+  --without-sidlx
+
 make all -j$CPU_COUNT
 make install
 
